e5c0737afadf9175490e4d4edf13aaba6df3eb70
Groceat <106503127+Groceat@users.noreply.github.com>
2023-04-08 00:01:08 -0400

    Update discussion_13.py

--- discussion_13.py
+++ discussion_13.py
@@ -16,6 +16,14 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
+    data = setUpDatabase("employee.json")
+
+
+    cur.execute("CREATE TABLE employee (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, hire_date TEXT, job_id INTEGER, salary INTEGER)")
+    count = 0
+    for emp in data:
+        cur.execute("INSERT INTO employee VALUES (?,?,?,?,?)",(count,emp["first_name"],emp["last_name"],emp["hire_date"],emp["job_id"],emp["salary"]))
+    conn.commit()
     pass
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
@@ -31,15 +39,36 @@ def add_employee(filename, cur, conn):
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
-    pass
+    cur.execute("SELECT Employees.hire_date, Jobs.job_title FROM Employees INNER JOIN Jobs ON Employees.job_id = Jobs.job_id")
+    done = cur.fetchall()
+    jobfin = None
+    jobdate = 100000
+    for job in done:
+        if int(job[0])<jobdate:
+            jobdate=int(job[0])
+            jobfin = job
+    
+
+    
+    return jobfin
+
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
+    cur.execute("SELECT Jobs.min_salary, Jobs.max_salary, Employees.first_name Employees.last_name FROM Employees INNER JOIN Jobs ON Employees.job_id = Jobs.job_id")
+    done = cur.fetchall()
+    employeees = []
+    for sals in done:
+        if (sals[0]-sals[1])<0:
+            employeees.append(sals[2],sals[3])
+    return employeees
+
     pass
 
 # TASK 4: VISUALIZATION
 def visualization_salary_data(cur, conn):
+    
     pass
 
 class TestDiscussion12(unittest.TestCase):
